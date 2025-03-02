89c3b80c4c3db4d1101ae10ec94bbfcdf142a30a
dmarroquin2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-03-01 18:40:29 -0600

    Se crea las funciones para listar las empresas.

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -57,3 +57,122 @@ export const updateCompany = async (req, res) => {
         })
     }
 }
+
+export const listCompanies = async (req, res) => {
+    try {
+
+        const companies = await Company.find()
+
+        res.status(200).send(
+            {   
+                success: true, 
+                message: 'Companies',
+                companies 
+            }
+        )
+    } catch (error) {
+        res.status(500).send(
+            { 
+                success: false, 
+                message: 'Intenal server error' 
+            }
+        )
+    }
+}
+
+export const listCompaniesByExperience = async (req, res) => {
+    try {
+       
+        const companies = await Company.find()
+            .sort({ yearsOfExperience: 1 })
+
+        res.status(200).send({
+            success: true,
+            message: 'Companies listed by years of experience',
+            companies
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Internal server error'
+        })
+    }
+}
+
+export const listCompaniesByCategory = async (req, res) => {
+    const { category } = req.params
+
+    try {
+        
+        if (!category) {
+            return res.status(400).send({
+                success: false,
+                message: 'Please provide a business category to filter.'
+            })
+        }
+
+        // Validar que sea permitida la vetgorua aunque no encontre como hacerlo de otra forma
+        const validCategories = ['Microempresa', 'Pequeña', 'Mediana', 'Grande']
+       
+        if (!validCategories.includes(category)) {
+            return res.status(400).send({
+                success: false,
+                message: 'Invalid business category provided.'
+            })
+        }
+
+        
+        const companies = await Company.find({ businessCategory: category })
+            .sort({ yearsOfExperience: -1 }) // Ordenar de mayor a menor porque sino da un error ya que no sabe    
+                                            // como ordenarlos
+
+        res.status(200).send({
+            success: true,
+            message: `Companies listed by business category: ${category}`,
+            companies
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Internal server error'
+        })
+    }
+}
+
+export const listCompaniesAlphabeticallyAZ = async (req, res) => {
+    try {
+
+        const companies = await Company.find()
+            .sort({ name: 1 }) // Ordenar de A-Z
+
+        res.status(200).send({
+            success: true,
+            message: 'Companies listed alphabetically A-Z',
+            companies
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Internal server error'
+        })
+    }
+}
+
+export const listCompaniesAlphabeticallyZA = async (req, res) => {
+    try {
+        
+        const companies = await Company.find()
+            .sort({ name: -1 })// Ordenar de Z-A
+
+        res.status(200).send({
+            success: true,
+            message: 'Companies listed alphabetically Z-A',
+            companies
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Internal server error'
+        })
+    }
+}
--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
-import { saveCompany, updateCompany } from "./company.controller.js";
+import { listCompanies, listCompaniesAlphabeticallyAZ, listCompaniesAlphabeticallyZA, listCompaniesByCategory, listCompaniesByExperience, saveCompany, updateCompany } from "./company.controller.js";
 import { validateJwt } from "../../middlewares/validate.jwt.js";
+import { saveCompanyValidator, saveCompanyValidator2 } from "../../middlewares/validators.js";
+import { objectIdValid } from "../../utils/db.validators.js";
 
 const api = Router()
 
-api.post('/', validateJwt ,saveCompany)
-api.post('/update/:id', validateJwt, updateCompany)
+api.post('/', validateJwt, saveCompanyValidator,saveCompany)
+
+api.get('/', validateJwt, listCompanies)
+
+api.get('/years/',validateJwt,listCompaniesByExperience)
+
+api.get('/listbycategory/:category', validateJwt, listCompaniesByCategory)
+
+api.get('/listalphabeticallyaz', validateJwt, listCompaniesAlphabeticallyAZ)
+
+api.get('/listalphabeticallyza', validateJwt, listCompaniesAlphabeticallyZA);
+
+api.put('/update/:id', validateJwt,objectIdValid,saveCompanyValidator2, updateCompany)
 
 export default api
